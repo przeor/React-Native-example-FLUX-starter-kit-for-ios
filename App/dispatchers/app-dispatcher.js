bf294e2b896277b1f9775dd22a3e2324b09725e3
Kamil Przeorski <kamil@przeorski.pl>
2015-04-02 18:21:40 +0200

    added dispatchers code

---
+++ App/dispatchers/app-dispatcher.js
@@ -0,0 +1,14 @@
+/** @jsx React.DOM */
+var Dispatcher = require('./dispatcher.js');
+var merge  = require('react/lib/merge');
+
+var AppDispatcher = merge(Dispatcher.prototype, {
+  handleViewAction: function(action){
+    this.dispatch({
+      source: 'VIEW_ACTION',
+      action: action
+    })
+  }
+})
+
+module.exports = AppDispatcher;
